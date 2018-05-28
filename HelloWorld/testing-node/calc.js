438339b69acc48afd195ccbdd9ea2e6a909acc06
Asaad Mahmood <asaad@mattermost.com>
2018-05-28 15:59:26 +0500

    Updating projects

--- HelloWorld/testing-node/calc.js
+++
@@ -1,8 +0,0 @@
-function add(num1, num2) {
-    return num1 + num2;
-}
-
-// Unless I export it, the add would remain restricted to this file.
-module.exports = {
-    addMethod: add
-}
--- HelloWorld/testing-node/main.js
+++
@@ -1,8 +0,0 @@
-const calculator = require('./calc');
-
-function main(){
-    const results = calculator.addMethod(3, 4);
-    console.log(results);
-}
-
-main();
